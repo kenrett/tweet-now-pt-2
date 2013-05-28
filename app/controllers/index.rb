80ce0d9b9127a00660a2c9f500ce3c1296ee6cd6
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-28 11:49:02 -0700

    initial commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,6 +4,7 @@ end
 
 get '/sign_in' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
+  session.delete(:request_token)
   redirect request_token.authorize_url
 end
 
@@ -19,7 +20,20 @@ get '/auth' do
   session.delete(:request_token)
 
   # at this point in the code is where you'll need to create your user account and store the access token
+  p @access_token
+  @user = User.find_or_create_by_username(@access_token.params[:screen_name])
+  @user.update_attributes(oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+  session[:user_id] = @user.id
 
   erb :index
-  
+end
+
+post '/post' do
+  @user = User.find(session[:user_id])
+  @twitter_user = Twitter::Client.new(
+    :oauth_token => @user.oauth_token,
+    :oauth_token_secret => @user.oauth_secret
+    )
+  @twitter_user.update(params[:tweet])
+  redirect '/'
 end
